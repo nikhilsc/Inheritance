c729e92b383fe00eed2c9b20512414a02e3bead8
nikhilsc <74759172+nikhilsc@users.noreply.github.com>
2020-11-20 13:47:34 +0530

    Update Q2Program.java

--- Q2Program.java
+++ Q2Program.java
@@ -1,1 +1,23 @@
+class Vehicle
+{
+	public void start()
+	{
+		System.out.println("in base class start");
+	}
+}
+class Fourwheeler extends Vehicle
+{
+	public void start()
+	{
+		System.out.println("in child class start");
+	}
+}
+public class Q2program {
 
+	public static void main(String[] args) {
+		Vehicle obj=new Fourwheeler();//upcasting
+		obj.start();//late binding
+
+	}
+
+}
